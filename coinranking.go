7c0f0d0e90040502c32586f7f313f74edcb5c262
Galih Muhammad <galih0muhammad@gmail.com>
2017-11-14 20:51:06 +0700

    add error handling for the request

--- coinranking.go
+++ coinranking.go
@@ -2,16 +2,21 @@ package main
 
 import (
   "fmt"
-
   "github.com/anaskhan96/soup"
+  "os"
 )
 
 func main() {
   url := "https://coinranking.com/"
   fmt.Println("Source :", url)
-  resp, _ := soup.Get(url)
+  response, error := soup.Get(url)
 
-  doc := soup.HTMLParse(resp)
+  // Add error handling, will terminate everything if error is not nil
+  if error != nil {
+    os.Exit(1)
+  }
+
+  doc := soup.HTMLParse(response)
   title := doc.Find("a", "class", "coin-list__body__row").Find("span", "class", "coin-name").Text()
 
   fmt.Println("Crypto name :", title)
